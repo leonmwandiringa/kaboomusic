977c5b43806df9a6b704df733af79466f0864c29
techadontech <tinashe.leon@yahoo.com>
2017-04-29 20:19:40 +0200

    the changes

--- index.php
+++ index.php
@@ -5,25 +5,54 @@
     <title> Home kaboomusic</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="">
+    <meta charset="utf-8">
     <meta name="description" content="">
     <link rel="stylesheet" href="">
+    <!--bootstrap 4 bro-->  
         <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="shortcut icon" href="">
+    
 </head>
 
 <body>
+    <!--navigation starts here-->
     <nav class="navbar navbar-inverse">
-        <div class="navbaer-header">
+        <div class="container">
+        <div class="navbar-header">
             <a href="index.php" class="navbar-brand">Kaboom</a>
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu1">
                 <span class="sr-obly">toggle navigation</span>
-                <span class=""
-            
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>    
             </button>
         </div>
+        <div class="navbar-form">
+            <form action="search.php"  method="post">
+                <div class="form-group">
+                    <label for="search" class="s-only">searh here</label>
+                    <div class="input-group">                  
+                        <input type="search" class="form-control" name="search" placeholder="search here">
+                        <span class="input-group-btn">
+                            <button class="btn btn-outline-secondary" name="submitsearch">
+                                <i class="glyphicon glyphicon-search"></i>
+                            </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="collapse" id="menu1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="index.php">Singles</a></li>
+                <li><a href="index.php">Mixtapes</a></li>
+                <li><a href="index.php">Albums</a></li>
+            </ul>
+        
+        </div>
+        </div>
     </nav>
-
+    <!--navigation end-->
 
 
 
